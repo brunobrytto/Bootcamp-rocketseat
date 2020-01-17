373d9d87b2e015d82f3b451406c3c5d193f4e117
Bruno Brito <bruno.b.souza@outlook.com>
2020-01-16 23:37:35 -0300

    Criando nova rota

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,5 +5,8 @@ const routes = new Router();
 routes.get('/',(req,res) =>{
   return res.json({message:'hello world'});
 })
+routes.get('/users',(req,res) =>{
+  return res.json({message:'hello bruno'});
+})
 
 export default routes;
